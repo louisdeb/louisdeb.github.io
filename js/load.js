291a16ad0867df5800db86f05ac7ee53f8c2fb51
Louis de Beaumont <debeaumont.louis@googlemail.com>
2015-05-09 21:04:33 +0100

    Waves working
    Waves being produced. Can currently click on circle before it is in
    position.

--- js/load.js
+++ js/load.js
@@ -5,21 +5,42 @@ $(function() {
 })
 
 function slideUpCircle() {
-    console.log('slideUpCircle called');
-
     var circle = $('#circle');    
     circle.animate({
-        marginTop : '-=30%'
+        top : '-=50%'
     }, 3000, 'easeOutQuint');
 }
 
-function waitForClick() {
-    console.log('waitForClick called');
-
+function waitForClick() {    
     var circle = $('#circle');
+    var parent = $('#centre-wrapper');
     var n = 0;
-    circle.click(function() {
-        console.log('clicked' + n);
+
+    circle.click(function() {        
         n++;
+
+        parent.append("<div class='hollow-circle'></div>");
+        var hollowCircle = $(".hollow-circle");
+
+        hollowCircle.animate({
+            width : '+=2500px',
+            height : '+=2500px',
+            top : '-=1250px',
+            left : '-=1250px'
+        }, 2000, function() {
+            n--;
+            hollowCircle.remove();
+        });
+
+        if(n > 12) {
+            
+            circle.remove();    
+            turnWhite();
+        }
+
     })
+}
+
+function turnWhite() {
+
 }
