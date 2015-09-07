f0c88cedbe27e6f47229fc24c577cc99232eb7f6
Louis de Beaumont <louis@Louiss-MBP.home>
2015-09-07 19:57:22 +0100

    Scrolling clean up and progress
    Smooth scroll to home using nav-home button. showSection function added
    to remove repeated code

--- web/portfolio/js/main.js
+++ web/portfolio/js/main.js
@@ -4,28 +4,38 @@ $(function() {
   //nav-home listener
   $('#nav-home').click(function() {
     console.log('nav-home click');
-    $('.section').css('display', 'none'); //remove jump using a delay
+    setTimeout(function() {
+      $('.section').css('display', 'none');
+    }, 500); //Delay stops jumping to home immediately
   });
 
   //nav-portfolio listener
   $('#nav-portfolio').click(function() {
     console.log('nav-portfolio click');
-    $('#portfolio').css('display', 'block');
+    showSection('#portfolio');
   });
 
   //nav-about listener
   $('#nav-about').click(function() {
     console.log('nav-about click');
-    $('#about').css('display', 'block');
+    showSection('#about');
   });
 
   //nav-contact listener
   $('#nav-contact').click(function() {
     console.log('nav-contact click');
-    $('#contact').css('display', 'block');
+    showSection('#contact');
   });
 
   //TODO: make any visible .section invisible when user scrolls back to title screen.
   //TODO: or don't allow user to scroll back to title screen
 
 });
+
+function showSection(sectionName) {
+  $('body').css('overflow', 'visible');
+  $(sectionName).css('display', 'block');
+  setTimeout(function() {
+    $('body').css('overflow', 'hidden');
+  }, 500);
+}
