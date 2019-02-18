fac00397df107f7fa8a3205b51a820fe2ee44782
Louis de Beaumont <debeaumont.louis@gmail.com>
2019-02-18 16:03:55 +0000

    Change image on load and slide sections

--- scripts/expander.js
+++ scripts/expander.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
   $('.album-title').click(titleClicked);
+  $('.album-content').each(function(i, o) {
+    if(i != 0)
+      $(this)[0].style.height = 0;
+  });
+  $('.album-title')[0].click();
 });
+
+function changePhoto(id) {
+  var photo1 = $('#photo1');
+  var photo2 = $('#photo2');
+
+  photo1.hide();
+  photo1.bind('load', function() {
+    photo1.fadeIn(function() {
+      photo2.attr('src', 'images/' + id + '.png')
+    });
+  }).attr('src', 'images/' + id + '.png');
+}
 
 function titleClicked(elem) {
   var id = elem.currentTarget.parentElement.id;
@@ -9,10 +26,11 @@ function titleClicked(elem) {
   $('.album-content').each(function(i, o) {
     var content = $(this)[0];
     var contentId = content.parentElement.id;
-    content.hidden = contentId != id;
+    if(contentId != id)
+      content.style.height = 0;
+    else
+      content.style.height = content.scrollHeight + 'px';
   });
 
-  // Set photo to relevant
-  // Might choose to use an external func and have some fade
-  $('#photo').attr('src', 'images/' + id + '.png');
+  changePhoto(id);
 }
